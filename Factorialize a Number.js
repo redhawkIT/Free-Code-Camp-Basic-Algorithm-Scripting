b9ca325c93fe43a1c2eba749e8a2d75771fc60d2
redhawkIT <khodkodmitry10011983@hotmail.com>
2016-01-28 18:28:48 -0600

    Free Code Camp Basic Algorithm Scripting

---
+++ Factorialize a Number.js
@@ -0,0 +1,29 @@
+// Return the factorial of the provided integer.
+
+// If the integer is represented with the letter n, a factorial is the product of all positive integers less than or equal to n.
+
+// Factorials are often represented with the shorthand notation n!
+
+// For example: 5! = 1 * 2 * 3 * 4 * 5 = 120
+
+// Recursively
+function factorialize(num) {
+  if (num === 0) {
+    return 1;
+  } else {
+    return num * factorialize(num - 1);
+  }
+}
+
+function factorialize(num) {
+  if (num === 0) {
+    return 1;
+  }
+  for (var i = num - 1; i > 1; i--) {
+    num *= i;
+  }
+  return num;
+}
+
+factorialize(5); // 120
+factorialize(0); // should return 1.
---
+++ Reverse a String.js
@@ -0,0 +1,10 @@
+// Reverse a String
+// Reverse the provided string.
+
+function reverseString(string) {
+
+  return string
+    .split('')
+    .reverse()
+    .join('');
+}
